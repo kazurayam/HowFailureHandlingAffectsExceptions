cee6cce2f792978529e7c77728a0cdb8bf667f6b
kazurayam <kazuaki.urayama@gmail.com>
2024-12-03 21:30:28 +0900

    first commit

---
+++ Keywords/com/kazurayam/ksbackyard/ThrowableUtils.groovy
@@ -0,0 +1,25 @@
+package com.kazurayam.ksbackyard
+
+import com.kms.katalon.core.exception.StepFailedException
+
+public class ThrowableUtils {
+
+	public static Throwable getCauseExceptStepFailedException(Throwable t) {
+		return getCauseExcept(t, StepFailedException.class)
+	}
+
+	private static Throwable getCauseExcept(Throwable t, Class<Throwable> except) {
+		assert t != null
+		if (t.getCause() != null) { 
+			Throwable cause = t.getCause()
+			println "*** " + cause.getClass().getName()
+			if (cause.getClass() != except) {
+				return cause
+			} else {
+				return getCauseExcept(cause, except)
+			}
+		} else {
+			return t
+		}
+	}
+}
---
+++ Scripts/TC1/Script1733227766159.groovy
@@ -0,0 +1,36 @@
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kazurayam.ksbackyard.ThrowableUtils
+
+TestObject makeTestObject(String id, String xpath) {
+	TestObject tObj = new TestObject(id)
+	tObj.addProperty("xpath", ConditionType.EQUALS, xpath)
+	return tObj
+}
+
+WebUI.openBrowser("http://demoaut-mimic.kazurayam.com/")
+TestObject validButton = makeTestObject("Make Appointment button",
+	"//a[@id='btn-make-appointment']")
+
+println "the case of CONTINUE_ON_FAILURE started"
+try {
+	WebUI.verifyElementNotPresent(validButton, 3, FailureHandling.CONTINUE_ON_FAILURE)
+} catch (Exception e) {
+	Throwable cause = ThrowableUtils.getCauseExceptStepFailedException(e)
+	println(">>> the case of CONTINUE_ON_FAILURE caught an Exception: "
+		+ cause.getCause().getClass().getName() + ": "+ cause.getMessage())
+}
+println "the case of CONTINUE_ON_FAILURE finished"
+
+println "the case of STOP_ON_FAILURE started"
+try {
+	WebUI.verifyElementNotPresent(validButton, 3, FailureHandling.STOP_ON_FAILURE)
+} catch (Exception e) {
+	Throwable cause = ThrowableUtils.getCauseExceptStepFailedException(e)
+	println(">>> the case of STOP_ON_FAILURE, caught an Exception: "
+		+ cause.getCause().getClass().getName() + ": " + cause.getMessage())
+}
+println "the case of STOP_ON_FAILURE finished"
