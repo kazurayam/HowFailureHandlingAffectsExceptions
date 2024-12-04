0646f153deddb67b545155d2a395032f0cded8cb
kazurayam <kazuaki.urayama@gmail.com>
2024-12-04 19:34:48 +0900

    udpated TC2; TC2 throws a StaleElementReferenceException and manage it

--- Scripts/TC2/Script1733285851173.groovy
+++ Scripts/TC2/Script1733285851173.groovy
@@ -1,10 +1,14 @@
 import java.nio.file.Path
 import java.nio.file.Paths
 
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+
 import com.kms.katalon.core.configuration.RunConfiguration
 import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.testobject.ConditionType
 import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
 
@@ -22,17 +26,19 @@ String urlString = htmlURL.toExternalForm()
 WebUI.openBrowser('')
 WebUI.navigateToUrl(urlString)
 WebUI.setViewPortSize(800, 600)
+WebDriver driver = DriverFactory.getWebDriver()
 
-TestObject btn = makeTestObject("btn", "//button[@id='btn']")
-
-WebUI.verifyElementPresent(btn, 10, FailureHandling.STOP_ON_FAILURE)
-
-WebUI.delay(5)
-
-WebUI.click(btn, FailureHandling.CONTINUE_ON_FAILURE)
+TestObject btnTO = makeTestObject("btn", "//button[@id='btn']")
+WebUI.verifyElementPresent(btnTO, 10, FailureHandling.STOP_ON_FAILURE)
+WebElement btnELM = WebUI.findWebElement(btnTO, 10, FailureHandling.STOP_ON_FAILURE)
 
 WebUI.delay(5)
 
-WebUI.click(btn, FailureHandling.STOP_ON_FAILURE)
+try {
+	btnELM.click()
+} catch (Exception e) {
+	println "An Exceptio was thrown, caught it by try {} catch () {} : " + e.getMessage()
+}
+WebUI.delay(5)
 
 WebUI.closeBrowser()
