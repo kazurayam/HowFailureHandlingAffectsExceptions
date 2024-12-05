fa878c4681d167f1ecf7c346bc82a0bd60268ea6
kazurayam <kazuaki.urayama@gmail.com>
2024-12-05 14:42:46 +0900

    updated TC3

--- Scripts/TC3/Script1733313726955.groovy
+++ Scripts/TC3/Script1733313726955.groovy
@@ -1,41 +1,51 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
 import java.nio.file.Path
 import java.nio.file.Paths
 
-import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 
 import com.kms.katalon.core.configuration.RunConfiguration
 import com.kms.katalon.core.model.FailureHandling
-import com.kms.katalon.core.testobject.ConditionType
 import com.kms.katalon.core.testobject.TestObject
-import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
+/**
+ * TC3
+ *
+ * A variation derived from the TC2.
+ * 
+ * This script demonstrates that FailureHandling.CONTINUE_ON_FAILURE makes 
+ * all keywords silent.
+ * No exception will be raised by a keyword invokation.
+ * Your Test Case script can not catch any Exception.
+ * 
+ * @author kazurayam
+ */
 Path projectDir = Paths.get(RunConfiguration.getProjectDir())
 Path html = projectDir.resolve("targetPage.html")
 URL htmlURL = html.toFile().toURI().toURL()
 String urlString = htmlURL.toExternalForm()
+WebUI.comment("navigating to " + urlString)
 
-// open a browser, navigate to the target web page
 WebUI.openBrowser('')
 WebUI.navigateToUrl(urlString)
 WebUI.setViewPortSize(800, 600)
-WebDriver driver = DriverFactory.getWebDriver()
 
-TestObject btnTO = findTestObject("myButton")
+TestObject myButtonTestObject = findTestObject("Object Repository/myButton")
 
-WebUI.verifyElementPresent(btnTO, 10, FailureHandling.STOP_ON_FAILURE)
-
-WebElement btnELM = WebUI.findWebElement(btnTO, 10, FailureHandling.STOP_ON_FAILURE)
-
-WebUI.delay(5)
+WebUI.verifyElementPresent(myButtonTestObject, 10, FailureHandling.STOP_ON_FAILURE)
 
 try {
-	//btnELM.click()
-	WebUI.findWebElement(btnTO, 10, FailureHandling.STOP_ON_FAILURE)
+	WebUI.verifyElementNotPresent(myButtonTestObject,
+								10,
+								FailureHandling.CONTINUE_ON_FAILURE)
+	// The keyword will throw no Exception
 } catch (Exception e) {
+	// You can not catch SERE here
 	println ">>> An Exception was caught: " + e.getClass().getName() + ": " + e.getMessage() + " <<<"
 }
-WebUI.delay(5)
 
 WebUI.closeBrowser()
+
+// In the end, in the Console, you will find a long Stack trace of StepFailedException is printed.
